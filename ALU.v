308342f5790298c47ce3f17141c9f2c0c2c82c1b
Noé <159713479+retasquid@users.noreply.github.com>
2025-05-26 21:24:27 +0200

    changed spi and IO moduls

---
+++ ALU.v
@@ -0,0 +1,74 @@
+module ALU (
+    output reg [15:0] S,
+    output reg [3:0] FLAGS,
+    input wire [15:0] A,
+    input wire [15:0] B,
+    input wire [2:0] OPALU,
+    input wire enFLAGS,
+    input wire clk,
+    input wire rst
+);
+    // Registres temporaires pour calculer correctement les drapeaux
+    reg carry;
+    reg overflow;
+    
+    // Fonction combinatoire pour calculer le résultat et les drapeaux
+    always @(*) begin
+        case (OPALU)
+            3'b000: begin // Addition
+                {carry, S} = {1'b0, A} + {1'b0, B};
+                // Overflow se produit quand les signes de A et B sont les memes, mais différents de result
+                overflow = (carry ^ S[15]);
+            end
+            3'b001: begin // Soustraction
+                {carry, S} = {1'b0, A} - {1'b0, B};
+                // Overflow se produit quand les signes de A et -B sont les memes, mais différents de result
+                overflow =  (carry ^ S[15]);
+            end
+            3'b010: begin // Décalage à gauche
+                S = A << B[3:0];
+                carry = (B[3:0] > 0 && B[3:0] <= 16) ? ((A >> (16 - B[3:0])) & 1'b1) : 1'b0;
+                overflow = 1'b0; // Généralement pas applicable pour les décalages
+            end
+            3'b011: begin // Décalage à droite
+                S = A >> B[3:0];
+                carry = (B[3:0] > 0 && B[3:0] <= 16) ? ((A >> (B[3:0] - 1)) & 1'b1) : 1'b0;
+                overflow = 1'b0; // Généralement pas applicable pour les décalages
+            end
+            3'b100: begin // AND logique
+                S = A & B;
+                carry = 1'b0;
+                overflow = 1'b0;
+            end
+            3'b101: begin // NAND logique
+                S = ~(A & B);
+                carry = 1'b0;
+                overflow = 1'b0;
+            end
+            3'b110: begin // OR logique
+                S = A | B;
+                carry = 1'b0;
+                overflow = 1'b0;
+            end
+            3'b111: begin // XOR logique
+                S = A ^ B;
+                carry = 1'b0;
+                overflow = 1'b0;
+            end
+        endcase
+    end
+    
+    // Mise à jour de S et des drapeaux sur front d'horloge
+    always @(negedge clk or posedge rst) begin
+        if (rst) begin
+            FLAGS <= 4'b0001; // Reset: mettre à 1 uniquement le drapeau de reset
+        end else begin
+            if (enFLAGS) begin
+                FLAGS[0] <= 1'b1;              // Bit de reset toujours à 1 après le reset
+                FLAGS[1] <= (S == 16'h0000); // Drapeau zéro
+                FLAGS[2] <= carry;             // Drapeau de retenue
+                FLAGS[3] <= overflow;          // Drapeau de dépassement
+            end
+        end
+    end
+endmodule
---
+++ UC.v
@@ -0,0 +1,60 @@
+module UC(
+    output wire PCpp,
+    output wire JMP,
+    output wire ret,
+    output wire Wreg,
+    output wire [1:0] Sregin,
+    output wire SBalu,
+    output wire ENflag,
+    output wire [2:0] OPalu,
+    output wire Wbus,
+    input wire [4:0] OPCode,
+    input wire [1:0] A,
+    input wire [3:0] FLAG,
+    input wire CLK,
+    input wire interrupt,
+    input wire CallInt
+   );
+    reg [11:0] UCrom [0:24];
+    reg [11:0] temp;
+    initial begin
+        UCrom[0]  = 12'h000;  
+        UCrom[1]  = 12'h940; 
+        UCrom[2]  = 12'h910;  
+        UCrom[3]  = 12'h930;  
+        UCrom[4]  = 12'h912;  
+        UCrom[5]  = 12'h932;  
+        UCrom[6]  = 12'h914; 
+        UCrom[7]  = 12'h934; 
+        UCrom[8]  = 12'h916; 
+        UCrom[9]  = 12'h936;
+        UCrom[10] = 12'h918; 
+        UCrom[11] = 12'h938;  
+        UCrom[12] = 12'h91a;  
+        UCrom[13] = 12'h93a; 
+        UCrom[14] = 12'h91c;  
+        UCrom[15] = 12'h93c; 
+        UCrom[16] = 12'h91e;  
+        UCrom[17] = 12'h93e;
+        UCrom[18] = 12'h400;  
+        UCrom[19] = 12'h980;  
+        UCrom[20] = 12'h801; 
+        UCrom[21] = 12'h821; 
+        UCrom[22] = 12'h9c0; 
+        UCrom[23] = 12'h600; 
+        UCrom[24] = 12'h9a0; 
+    end
+
+    always @(*) begin
+        temp<=UCrom[OPCode];
+    end
+        assign ret=temp[9];
+        assign Wreg=temp[8];
+        assign Sregin=temp[7:6];
+        assign SBalu=temp[5];
+        assign ENflag=temp[4];
+        assign OPalu=temp[3:1];
+        assign Wbus=temp[0] | CallInt;
+        assign JMP=temp[9]|(temp[10]&FLAG[A]);
+        assign PCpp=~interrupt & temp[11];
+endmodule
